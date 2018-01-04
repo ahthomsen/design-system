3edfa83bc0ddfb5652cc40f28ff851927f2820a1
Anders Thomsen <ahthomsen@gmail.com>
2018-01-04 17:08:51 +0100

    setting up the basic structure

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>NoMore Design Sysmte</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="{{ mix('css/app.css')}}" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="navigation-top">
+            <h1>The NoMore Design System</h1>
+        </div>
+
+        <div class="left-menu-container">
+            <a class="left-menu-link" href="#typography">Typography</a>
+        </div>
+
+        <div class="main-container-content">
+            <h2>Typography</h2>
+            <p>Here is some text</p>
+
+        </div>
+    </body>
+</html>
