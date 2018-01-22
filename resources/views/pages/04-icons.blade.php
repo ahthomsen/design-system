fd7eb03681ab29571ea603ba18500cf45673cbc7
Anders Thomsen <ahthomsen@gmail.com>
2018-01-22 07:00:52 +0100

    added to todo and icons

--- resources/views/pages/04-icons.blade.php
+++ resources/views/pages/04-icons.blade.php
@@ -1,22 +1,150 @@
 <h1>Using icons</h1>
 <h3>Introduction</h3>
 <p>Icons play a big role in our world. Why this must be done properly.</p>
+<h3>General icon guidelines to follow:</h3>
+<ul class="list list-unordered">
+  <li class="list-item">Only use line icons - with the same line width</li>
+  <li class="list-item">Only use icons within the main color scheme</li>
+  <li class="list-item">Keep icons simple - do not include more advanced icons unless part of a larger illustration</li>
+  <li class="list-item">Only "complete" line icons</li>
+  <li class="list-item">Ensure high contrast when using backgrounds</li>
+</ul>
 <h3>Small icons</h3>
 <p>The style of icons differ quite a lot dependent on the size - and this needs to be be considered when working with their style.</p>
 
 <div class="px-1 mb-4">
   <div class="flex -mx-1">
-    <div class="w-1/2 px-1">
-      	<blockquote class="example example-correct">
-      		<img src="">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-small" src="/img/target.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+      	 <blockquote class="example example-correct">
+      		  <img class="icon-small" src="/img/growth.svg">
       	</blockquote>
     </div>
-    <div class="w-1/2 px-1">
-      	 <blockquote class="example example-error">
-      		<img src="">
-      	</blockquote>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-small" src="/img/rocket-purple.svg">
+        </blockquote>
+    </div>
+  </div>
+</div>
+
+<div class="px-1 mb-4">
+  <div class="flex -mx-1">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-small-inverse bg-purple-darker" src="/img/ipo.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-small-inverse bg-pink" src="/img/person-white.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct shadow">
+            <img class="icon-small-inverse bg-grey-light" src="/img/rocket-purple.svg">
+        </blockquote>
     </div>
   </div>
 </div>
 
 <div class="clearfix"></div>
+
+<h3>Normal icons</h3>
+<p>The style of icons differ quite a lot dependent on the size - and this needs to be be considered when working with their style.</p>
+
+<div class="px-1 mb-4">
+  <div class="flex -mx-1">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon" src="/img/target.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon" src="/img/growth.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon" src="/img/rocket-purple.svg">
+        </blockquote>
+    </div>
+  </div>
+</div>
+
+<div class="px-1 mb-4">
+  <div class="flex -mx-1">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-inverse bg-purple-darker" src="/img/ipo.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-inverse bg-pink" src="/img/person-white.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-correct">
+            <img class="icon-inverse bg-grey-light" src="/img/rocket-purple.svg">
+        </blockquote>
+    </div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+
+<h3>Examples of good icon packages</h3>
+<ul>
+  <li class="list-item"><a href="https://www.flaticon.com/packs/startup-19">Startup pack from flaticon</a></li>
+</ul>
+
+<h3 class="mt-8">Examples of not accepted icons</h3>
+<p>Sometimes it can be helpful to understand which types of icons that are not to be used. Thus the following illustrate different examples of icon styles that should not be used:</p>
+
+<p class="-mb-2">Wrong icon types:</p>
+<div class="px-1 mb-4">
+  <div class="flex -mx-1">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+            <img class="icon" src="/img/house-fat-line.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+            <img class="icon" src="/img/house-solid.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+            <img class="icon" src="/img/house-color.svg">
+        </blockquote>
+    </div>
+  </div>
+</div>
+
+<p class="-mb-2">Wrong color usage:</p>
+<div class="px-1 mb-4">
+  <div class="flex -mx-1">
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+            <img class="icon" src="/img/rocket-black.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+            <img class="icon-inverse bg-purple" src="/img/rocket-purple.svg">
+        </blockquote>
+    </div>
+    <div class="w-1/3 px-1">
+         <blockquote class="example example-error">
+           <img class="icon-inverse bg-pink" src="/img/rocket-purple.svg">
+        </blockquote>
+    </div>
+  </div>
+</div>
---
+++ resources/views/pages/TODO.blade.php
@@ -0,0 +1,12 @@
+<h1>What needs to be build</h1>
+
+<p>We need to design and build the following items:</p>
+
+<ul>
+	<li>Modals</li>
+	<li>Forms (including dropdowns, labels etc)</li>
+	<li>Tabs</li>
+	<li>Drop down buttons</li>
+	<li>Anchor / links</li>
+	<li>List items</li>
+</ul>
